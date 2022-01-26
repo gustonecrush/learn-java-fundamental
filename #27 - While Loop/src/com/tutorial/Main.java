31c9a73f6a6046ac2cd5bc4e45f9abb0e7bb639f
gustonecrush <98437617+gustonecrush@users.noreply.github.com>
2022-01-27 00:12:09 +0700

    Adding While Loop Directory

---
+++ #27 - While Loop/src/com/tutorial/Main.java
@@ -0,0 +1,29 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        int a = 0;
+//
+//        while(a > -10) {
+//            System.out.println(a);
+//            a--;
+//        }
+
+//        int a = 0;
+//        do {
+//            System.out.println(a);
+//            a++;
+//        } while(a == 10);
+//        System.out.println(a);
+
+        for(byte i = 1; i > 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("finish");
+//        System.out.println(i); -> can't access, because java using block scope
+
+
+
+    }
+}

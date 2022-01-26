c46deef7e8e9dc1be120f0e58b9925f27ea08a2e
gustonecrush <98437617+gustonecrush@users.noreply.github.com>
2022-01-26 23:59:32 +0700

    Organize Program with Package

---
+++ Java #5 - Organize Program with Package/src/myprogram/OtongSurotong.java
@@ -0,0 +1,8 @@
+package src.myprogram;
+public class OtongSurotong {
+    
+    public static void main(String[] args) {
+        System.out.println("My name is Otong, My friends are Ucup");
+    }
+
+}

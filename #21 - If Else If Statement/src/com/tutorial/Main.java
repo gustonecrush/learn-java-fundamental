c91ffa7c1fd6e6f65ba9730ecd71de6ffea060a3
gustonecrush <98437617+gustonecrush@users.noreply.github.com>
2022-01-27 00:10:03 +0700

    If Else If Statement

---
+++ #21 - If Else If Statement/src/com/tutorial/Main.java
@@ -0,0 +1,22 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        // tutorial if else if statement
+        int a = 50;
+
+        System.out.println("Program Start");
+        // if else if statement
+
+        if( a == 5 ) {
+            System.out.println("this is action 1");
+        } else if( a == 1 ) {
+            System.out.println("this is action 2");
+        } else {
+            System.out.println("this is default action");
+        }
+
+        System.out.println("Program End");
+
+    }
+}

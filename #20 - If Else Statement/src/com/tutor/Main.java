e16bbbb7494216d45697c29caa83cfacc69ab638
gustonecrush <98437617+gustonecrush@users.noreply.github.com>
2022-01-27 00:09:45 +0700

    If Else Statement

---
+++ #20 - If Else Statement/src/com/tutor/Main.java
@@ -0,0 +1,18 @@
+package com.tutor;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("===== Conditional =====");
+
+        int data = 4;
+
+        System.out.println("data: " + data);
+        if(data == 5) {
+            System.out.println("💗");
+        } else {
+            System.out.println("🐺");
+        }
+        System.out.println("Finish");
+
+    }
+}

f91764daaf8174f043527918eccbc5864b0dd1fd
gustonecrush <98437617+gustonecrush@users.noreply.github.com>
2022-01-27 00:00:04 +0700

    Printing & Execution Flow

---
+++ #6 - Printing and Execution/src/com/tutorial/Main.java
@@ -0,0 +1,20 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // statement
+        System.out.print("2nd-line: Hello, Otong!");
+
+        System.out.print("3rd-line: this is third line!");
+        System.out.print("4th-line: i continue third line");
+
+        System.out.print("5th-line: Hello Makmun!");
+        System.out.println("6th-line: Hello Ucup!");
+
+        System.out.print("7th-line: Hai bro! \n");
+        System.out.print("8th-line: Kuy Kongkow! \n");
+        System.out.print("7th-line: Ke Ngenter! \n");
+
+        System.out.printf("wiro sableng %d naga geni %d", 100, 212);
+    }
+}

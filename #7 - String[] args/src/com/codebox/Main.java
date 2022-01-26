3089d8eb11768997f6df0bc2f703ea4b19144d17
gustonecrush <98437617+gustonecrush@users.noreply.github.com>
2022-01-27 00:02:24 +0700

    String Args

---
+++ #7 - String[] args/src/com/codebox/Main.java
@@ -0,0 +1,9 @@
+package com.codebox;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0]);
+        System.out.println("Hello " + args[1]);
+        System.out.println("Hello " + args[2]);
+    }
+}

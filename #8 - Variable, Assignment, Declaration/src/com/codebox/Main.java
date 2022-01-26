83c2a2d239e552daf43399e363b5503db9cccbec
gustonecrush <98437617+gustonecrush@users.noreply.github.com>
2022-01-27 00:02:52 +0700

    Variable, Assignment, Declaration

---
+++ #8 - Variable, Assignment, Declaration/src/com/codebox/Main.java
@@ -0,0 +1,18 @@
+package com.codebox;
+
+public class Main {
+    public static void main(String[] args) {
+        // this is a comment
+        // data type
+        int a = 10;
+        System.out.println("a value: " + a);
+        a = 20;
+        System.out.println("a new value: " + a);
+
+        // declaration
+        int b;
+
+        b = 7;
+        System.out.println("b value: " + b);
+    }
+}

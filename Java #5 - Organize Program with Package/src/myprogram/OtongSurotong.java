aae440ed1116972381845e1ebe4692d805c9cb81
gustonecrush <98437617+gustonecrush@users.noreply.github.com>
2022-01-27 00:00:56 +0700

    delete #5 Directory

--- Java #5 - Organize Program with Package/src/myprogram/OtongSurotong.java
+++
@@ -1,8 +0,0 @@
-package src.myprogram;
-public class OtongSurotong {
-    
-    public static void main(String[] args) {
-        System.out.println("My name is Otong, My friends are Ucup");
-    }
-
-}
